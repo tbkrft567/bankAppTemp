67ef13c07e8ffd14d36a0b109689a9b2312b138e
tbkrft567 <tbkrft567@gmail.com>
2020-01-29 15:32:01 -0600

    package.json changes:

--- server/config/sessionRoutes.js
+++ server/config/sessionRoutes.js
@@ -5,13 +5,7 @@ const sessions = require('../controllers/sessions.js')
 
 module.exports = function (app) {
 
-   app.post('/app/users/create', function (req, res) {
+   app.post('/app/sessions/create', function (req, res) {
       sessions.create(req, res);
    })
-
-   app.delete('/app/users/destroy/:id', function (req, res) {
-      console.log("***DELETE_IN_ROUTES_JS***")
-      sesssions.destroy(req, res);
-   })
-
 }
