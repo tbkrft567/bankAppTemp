cbfbb74635b21c40b7d038cd1f8d9b767f13b451
tbkrft567 <tbkrft567@gmail.com>
2020-01-30 09:16:41 -0600

    Public folders updated

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -2,6 +2,7 @@ console.log('****CONTROLLERS*****');
 
 const User = require('../models/user.js');
 const Account = require('../models/account.js');
+const Transaction = require('../models/transaction');
 
 module.exports = {
     index: function (req, res) {
@@ -33,18 +34,35 @@ module.exports = {
 
     create: function (req, res) {
         console.log(req.body);
-        Transaction.create(req.body)
-
-            .then(data => { console.log(data); res.json(data) })
-            .catch(err => {
-                console.log("**TRANSACTION_CREATE_ERROR**");
-                console.log(err);
-                for (var key in err.errors) {
-                    req.flash('registration', err.errors[key].message);
+        User.findOne({_id: req.params.userid})
+            .then(user => {
+                for(var i = 0; i < user.accounts.length; i++){
+                    if(user.accounts[i]._id == req.params.accountid){
+                        Transaction.create(req.body)
+                        .then(transaction => {
+                            Account.update({},{})
+                            .then({})
+                            .catch({})
+                            })
+                        .catch(err => console.log(err))
+                        
+                    }
                 }
-                console.log("***ERRORS HERE ***", err)
-                res.json({ errors: err });
-            });
+            })
+            .catch(err => console.log(err))
+
+        
+
+            // .then(data => { console.log(data); res.json(data) })
+            // .catch(err => {
+            //     console.log("**TRANSACTION_CREATE_ERROR**");
+            //     console.log(err);
+            //     for (var key in err.errors) {
+            //         req.flash('registration', err.errors[key].message);
+            //     }
+            //     console.log("***ERRORS HERE ***", err)
+            //     res.json({ errors: err });
+            // });
     },
 
     update: function (req, res) {
